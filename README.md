40afe4de9eb59d7cea6c37e3009ff25d56a76bb9
leonardoleano <leodrl9@gmail.com>
2024-05-09 23:13:41 -0300

    runing with communication but glom and map returning error

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 docker-compose up --scale spark-workers=3
+
+https://medium.com/@MarinAgli1/setting-up-a-spark-standalone-cluster-on-docker-in-layman-terms-8cbdc9fdd14b
+
+https://www.youtube.com/watch?v=mzQX2lg8FDs
+
+https://www.youtube.com/watch?v=FteThJ-YvXk
--- example.py
+++ example.py
@@ -1,14 +1,28 @@
-from pyspark import SparkConf
-from pyspark import SparkContext
+import findspark
+findspark.find()
+findspark.init()
 
-conf = SparkConf()
-conf.setMaster("spark://spark-master:7077")
-conf.setAppName("example")
-sc = SparkContext(conf=conf)
+
+from pyspark.sql import SparkSession
+
+# Create SparkSession
+spark = SparkSession.builder \
+                    .master("spark://172.22.0.2:7077") \
+                    .appName("example") \
+                    .getOrCreate()
+
+# Parallelize data and collect
+data = [0, 2, 3, 4, 6]
+rdd_a = spark.sparkContext.parallelize(data, 5)
+result = rdd_a.glom().collect() # glom ?
+
 
 def mod(x):
     return (x,x*x)
 
-rdd = sc.parallelize(range(2)).map(mod).take(10)
+rdd_b = spark.sparkContext.parallelize(range(2))
+result_b = rdd_b.map(mod) # map  returning error?
 
-print(rdd)
+# # rdd = spark.parallelize(range(2)).map(mod).take(10)
+
+# print(rdd)
